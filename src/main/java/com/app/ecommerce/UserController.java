58c3920b6b45b4bb7b27a7dedbb45a042e75eca7
Nazmul Hossain <nazmuljuit@gmail.com>
2025-06-29 14:31:04 +0600

    :pencil2: Add:=> Implement list and create api for users

--- src/main/java/com/app/ecommerce/UserController.java
+++ src/main/java/com/app/ecommerce/UserController.java
@@ -1,13 +1,24 @@
 package com.app.ecommerce;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
-
+    private List<User> userList = new ArrayList<>();
     @GetMapping("/api/users")
-    public String getAllUsers(){
-        return "";
+    public List<User> getAllUsers() {
+        return userList;
+    }
+
+    @PostMapping("/api/users")
+    private List<User> createUser(@RequestBody User user) {
+        userList.add(user);
+        return userList;
     }
 }
