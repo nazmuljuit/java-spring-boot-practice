5ce82fa4ea85114b5ba72d1c7e1e55fcc4952be5
Nazmul Hossain <nazmuljuit@gmail.com>
2025-07-01 16:47:37 +0600

    :pencil2: Add:=> Implement request mapping annotation

--- src/main/java/com/app/ecommerce/UserController.java
+++ src/main/java/com/app/ecommerce/UserController.java
@@ -9,30 +9,31 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/api/users")
+    @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
         return new ResponseEntity<>(userService.fetchAllUsers(),
                                                  HttpStatus.OK);
     }
 
-    @GetMapping("/api/users/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<User> getUser(@PathVariable Long id) {
 
         return userService.fetchUser(id).map(u -> new ResponseEntity<>(u, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    @PostMapping("/api/users")
+    @PostMapping
     private ResponseEntity<String> createUser(@RequestBody User user) {
         userService.addUser(user);
         return ResponseEntity.ok("User added successfully");
     }
 
-    @PutMapping("/api/users/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<String> updateUser(@PathVariable Long id, @RequestBody User user) {
        boolean updated = userService.updateUser(id, user);
        if (updated) {
